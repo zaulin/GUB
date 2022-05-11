39228e0b5a4b44000b835683ba15e683ff12e4ef
zaulin <zaulin@yahoo.com>
2022-05-11 03:06:17 +0200

    Add files via upload

---
+++ trixis/codigo.js
@@ -0,0 +1,104 @@
+const version = "0.9";
+const fecha = "23/04/2022";
+
+function clickBack() {
+    window.open('../index.html', "_self")
+}
+
+
+function buscar() {
+  var input, filter, table, tr, td, i, txtValue, iHits;
+
+  iHits = 0;
+
+  inputCodi = document.getElementById("myInputCodi");
+  filterCodi = normalice(inputCodi.value.toUpperCase());
+
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+
+  if (filterCodi != "") {
+
+    for (i = 0; i < tr.length; i++) {
+      hit = 0;
+      tdCodi = tr[i].getElementsByTagName("td")[0];
+      if (tdCodi) {
+        txtCodi = normalice(tdCodi.textContent || tdCodi.innerText);
+        if (txtCodi.toUpperCase().indexOf(filterCodi) > -1) {
+          hit = 1;
+        }
+
+        if (hit==1) {
+          tr[i].style.display = "";
+          iHits = iHits + 1;
+        } else {
+          tr[i].style.display = "none";
+        }
+      }
+    }
+
+  }
+  
+}
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+}
+
+function reset(){
+  document.getElementById("myInputCodi").value = "";
+  
+
+  buscar();
+}
+
+function esconde() {
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+
+  for (i = 0; i < tr.length; i++) {
+
+    for (j = 1; j < 3; j++) {
+      td = tr[i].getElementsByTagName("td")[j];
+      th = tr[i].getElementsByTagName("th")[j];
+      if (td) {
+            td.style.display = "none";
+      }
+      if (th) {
+            th.style.display = "none";
+      }  
+    }
+    
+  }
+
+  reset();
+}
+
+
+function off() {
+  document.getElementById("overlay").style.display = "none";
+}
+
+function tableClick(el) {
+  var indice, table, tr, td;
+  indice = $(el).closest('tr').index();
+
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+  td = tr[indice + 1].getElementsByTagName("td")
+
+  document.getElementById("popUpCodi").innerHTML = "<strong>" + td[0].innerText + "</strong>"
+  document.getElementById("popUpDataAlta").innerText = td[1].innerText; 
+  document.getElementById("popUpTipus").innerText = td[2].innerText;
+
+  document.getElementById("overlay").style.display = "block";
+}
+
+function normalice(text){
+  return text.normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+}
+
+function pageonload() {
+  document.getElementById("fecha").innerText = "v." + version + " - " + fecha;
+  esconde();
+}
