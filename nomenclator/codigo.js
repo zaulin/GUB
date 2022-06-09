a322df333777560828cf6a73e096ebcfa7cb2d34
zaulin <zaulin@yahoo.com>
2022-06-09 19:45:28 +0200

    Add files via upload

--- nomenclator/codigo.js
+++ nomenclator/codigo.js
@@ -1,8 +1,19 @@
 const version = "0.9";
 const fecha = "23/04/2022";
+const arrayDenuncies = [];
+const csvData = [];
 
 function clickBack() {
     window.open('../index.html', "_self")
+}
+
+function reset(){
+  document.getElementById("myInputCodi").value = "";
+  document.getElementById("myInput").value = "";
+  document.getElementById("myInput2").value = "";
+  document.getElementById("dropdownDenuncies").value = "";
+
+  buscar();
 }
 
 function keyupInput1(){
@@ -15,9 +26,17 @@ function keyupInput2(){
   buscar();
 }
 
+function keyupInputCodi(){
+  document.getElementById("myInput").value = "";
+  document.getElementById("myInput2").value = "";
+  document.getElementById("dropdownDenuncies").value = "";
+
+  buscar();
+}
+
 
 function buscar() {
-  var input, filter, table, tr, td, i, txtValue, iHits;
+  var input, filter, table, tr, td, i, txtValue, iHits, arrayFiltreDenuncies;
 
   iHits = 0;
   input = document.getElementById("myInput");
@@ -29,8 +48,15 @@ function buscar() {
   inputCodi = document.getElementById("myInputCodi");
   filterCodi = normalice(inputCodi.value.toUpperCase());
 
-  //logic = document.getElementById("button");
-  //operator = logic.innerText;
+  inputDenuncies = document.getElementById("dropdownDenuncies");
+  txtFilterDenuncies = inputDenuncies.value;
+
+  //Obtener los codigos de denuncia del dropdown
+  if (txtFilterDenuncies) {
+    if (txtFilterDenuncies != "") {
+      arrayFiltreDenuncies = arrayDenuncies[inputDenuncies.selectedIndex - 1].codis.split(",")
+    }
+  }
 
   table = document.getElementById("myTable");
   tr = table.getElementsByTagName("tr");
@@ -75,40 +101,47 @@ function buscar() {
 
         txtValue = normalice(td.textContent || td.innerText);
   
-          if (filter2 == "") {
-            if (txtValue.toUpperCase().indexOf(filter) > -1) {
-              hit = 1
-            } else {
-              hit = 0;
-            }  
-          } else {
-          
-              if (txtValue.toUpperCase().indexOf(filter) > -1 && txtValue.toUpperCase().indexOf(filter2) > -1) {
-                hit = 1
+          if (arrayFiltreDenuncies) {
+            tdCodi = tr[i].getElementsByTagName("td")[1];
+            txtCodi = normalice(tdCodi.textContent || tdCodi.innerText);
+            
+            if (arrayFiltreDenuncies.includes(txtCodi)) {
+              if (filter2 == "") {
+                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                  hit = 1
+                } else {
+                  hit = 0;
+                }  
               } else {
-                hit = 0;
-              } 
-            //miramos el operador
-            /*
-            if (operator == "i") {
-              //operador AND
-              if (txtValue.toUpperCase().indexOf(filter) > -1 && txtValue.toUpperCase().indexOf(filter2) > -1) {
-                hit = 1
-              } else {
-                hit = 0;
-              }  
+              
+                  if (txtValue.toUpperCase().indexOf(filter) > -1 && txtValue.toUpperCase().indexOf(filter2) > -1) {
+                    hit = 1
+                  } else {
+                    hit = 0;
+                  } 
+              }
 
-            } else {
-              //operador OR
-              if (txtValue.toUpperCase().indexOf(filter) > -1 || txtValue.toUpperCase().indexOf(filter2) > -1) {
+            } 
+          } else {
+
+            if (filter2 == "") {
+              if (txtValue.toUpperCase().indexOf(filter) > -1) {
                 hit = 1
               } else {
                 hit = 0;
               }  
+            } else {
+            
+                if (txtValue.toUpperCase().indexOf(filter) > -1 && txtValue.toUpperCase().indexOf(filter2) > -1) {
+                  hit = 1
+                } else {
+                  hit = 0;
+                } 
             }
-            */
 
           }
+
+
 
         if (hit==1) {
           tr[i].style.display = "";
@@ -129,34 +162,6 @@ function buscar() {
 
 function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-}
-
-function changeButton(){
-  var btn = document.getElementById("button");
-  if(btn.innerText=="i"){
-   btn.innerText="o";
-  }
-  else{
-    btn.innerText="i";
-  }
-  buscar();
-}
-
-function reset(param1, param2){
-  
-  if (param1) {
-    document.getElementById("myInput").value = param1;
-  } else {
-    document.getElementById("myInput").value = "";
-  }
-  
-  if (param2) {
-    document.getElementById("myInput2").value = param2;
-  } else {
-    document.getElementById("myInput2").value = "";
-  }
-
-  buscar();
 }
 
 function normalice(text){
@@ -190,12 +195,14 @@ function esconde() {
     
   }
 
+/*
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
   const paramFilter1 = urlParams.get('filter1')
   const paramFilter2 = urlParams.get('filter2')
 
   reset(paramFilter1, paramFilter2);
+*/
 }
 
 function off() {
@@ -244,5 +251,82 @@ function tableClick(el) {
 function pageonload() {
   document.getElementById("fecha").innerText = "v." + version + " - " + fecha;
   esconde();
+  loadDenuncias();
+
   document.getElementById("loading").style.display = "none";
+}
+
+function loadDenuncias() {
+
+  $.ajax({
+    type: "GET",
+    url: "denuncies.csv",
+    dataType: "text",
+    success: function(res) {
+
+      var count = 0; // cache the running count
+      csvArray = Papa.parse(res).data;
+
+      //TENEMOS UN ARRAY --> NECESITAMOS UN ARRAY DE OBJETOS!!
+      for (i = 1; i < csvArray.length; i++) {
+        csvData.push({});
+
+        for (j = 0; j < csvArray[0].length; j++) {
+          csvData[i - 1][csvArray[0][j]] = csvArray[i][j]
+        }
+
+      }
+
+      csvData.sort(function(a, b) {
+        if (a.grup) {
+          if (b.grup) {
+            return a.grup.localeCompare(b.grup) || a.nom.localeCompare(b.nom)
+          }
+        }
+      });
+
+
+      for (var i = 0; i < csvData.length; i++) {
+        arrayDenuncies.push(csvData[i])
+      }
+
+        $(function() {
+          var $select = $('#dropdownDenuncies');
+          var grupAnterior = "";
+          var group;
+          $.each(csvData, function() {
+            if (this.grup) {
+              if (this.grup != "") {
+
+                if (grupAnterior == this.grup) {
+
+                } else {
+                  group = $('<optgroup label="' + this.grup + '" />');
+                }
+
+                if (group) {
+                  $('<option />').html(this.nom).appendTo(group);
+                  group.appendTo($select);
+                }
+                
+                grupAnterior = this.grup;
+              }
+            }
+          });
+
+        });
+
+      
+    }
+
+  });
+
+
+}
+
+function dropdownChange() {
+  document.getElementById("myInputCodi").value = "";
+
+  buscar();
+
 }
