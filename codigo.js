a57cfb3cc6aa0e9963c6d6db21c9b3f327b479c2
zaulin <zaulin@yahoo.com>
2022-06-10 16:57:46 +0200

    Add files via upload

--- codigo.js
+++ codigo.js
@@ -1,33 +1,33 @@
 function clickNomenclator(){
-    document.getElementById("loading").style.display = "block";
+    document.getElementById("loadingBlack").style.display = "block";
     window.open("nomenclator/index.html","_self")
 }
 
 function clickActes(){
-    document.getElementById("loading").style.display = "block";
+    document.getElementById("loadingBlack").style.display = "block";
     window.open("actes/index.html","_self")
 }
 
 function clickTrixis(){
-    document.getElementById("loading").style.display = "block";
+    document.getElementById("loadingBlack").style.display = "block";
     window.open("trixis/index.html","_self")
 }
 
 function clickPermisos(){
-    document.getElementById("loading").style.display = "block";
+    document.getElementById("loadingBlack").style.display = "block";
     window.open("permisos/index.html","_self")
 }
 
 function clickVTC(){
-    document.getElementById("loading").style.display = "block";
+    document.getElementById("loadingBlack").style.display = "block";
     window.open("VTC/index.html","_self")
 }
 
 function clickNegra(){
-    document.getElementById("loading").style.display = "block";
+    document.getElementById("loadingBlack").style.display = "block";
     window.open("negra/index.html","_self")
 }
 
 function pageonload() {
-    document.getElementById("loading").style.display = "none";
+    document.getElementById("loadingBlack").style.display = "none";
 }
