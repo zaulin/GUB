83562542570eb4cdcab66b520553529a7fbbede9
zaulin <zaulin@yahoo.com>
2022-06-11 16:57:56 +0200

    Add files via upload

--- codigo.js
+++ codigo.js
@@ -30,4 +30,40 @@ function clickNegra(){
 
 function pageonload() {
     document.getElementById("loadingBlack").style.display = "none";
+
+    acceptCookies = getCookie("acceptCookies");
+    if (acceptCookies=="") {
+      document.getElementById("overlay").style.display = "block";
+    }
+    if (acceptCookies=="0") {
+      document.getElementById("overlay").style.display = "block";
+    }
+}
+
+function acceptCookiesClick(){
+    setCookie("acceptCookies", "1", 3600)
+    document.getElementById("overlay").style.display = "none";
+}
+
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
 }
