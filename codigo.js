0857fdf50c1d2ad4aec6674b7ed0ef021395e062
zaulin <zaulin@yahoo.com>
2022-06-15 23:17:07 +0200

    Add files via upload

--- codigo.js
+++ codigo.js
@@ -1,35 +1,43 @@
 function clickNomenclator(){
     document.getElementById("loadingBlack").style.display = "block";
+    document.getElementById("galetesDiv").style.display = "none";
     window.open("nomenclator/index.html","_self")
 }
 
 function clickActes(){
     document.getElementById("loadingBlack").style.display = "block";
+    document.getElementById("galetesDiv").style.display = "none";
     window.open("actes/index.html","_self")
 }
 
 function clickTrixis(){
     document.getElementById("loadingBlack").style.display = "block";
+    document.getElementById("galetesDiv").style.display = "none";
     window.open("trixis/index.html","_self")
 }
 
 function clickPermisos(){
     document.getElementById("loadingBlack").style.display = "block";
+    document.getElementById("galetesDiv").style.display = "none";
     window.open("permisos/index.html","_self")
 }
 
 function clickVTC(){
     document.getElementById("loadingBlack").style.display = "block";
+    document.getElementById("galetesDiv").style.display = "none";
     window.open("VTC/index.html","_self")
 }
 
 function clickNegra(){
     document.getElementById("loadingBlack").style.display = "block";
+    document.getElementById("galetesDiv").style.display = "none";
     window.open("negra/index.html","_self")
 }
 
 function pageonload() {
     document.getElementById("loadingBlack").style.display = "none";
+    document.getElementById("galetesDiv").style.display = "block";
+    
 
     acceptCookies = getCookie("acceptCookies");
     if (acceptCookies=="") {
