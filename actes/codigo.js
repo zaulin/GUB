ed94798097eaeb17c24832ecf5abaa776882b9ed
zaulin <zaulin@yahoo.com>
2022-10-10 09:42:39 +0200

    Add files via upload

--- actes/codigo.js
+++ actes/codigo.js
@@ -8,135 +8,18 @@ function clickBack() {
 }
 
 function keyupInput1() {
-  document.getElementById("dropdownSituacion").value = "";
-  document.getElementById("labelExtra").innerText = "";
-  document.getElementById("divTaulaPerjudicat").style.display = "none";
-  document.getElementById("divTaulaAutor").style.display = "none";
-  document.getElementById("textResta").style.display = "none";
-  document.getElementById("divTaulaResta").style.display = "block";
   buscar();
 }
 
 function keyupInput2() {
-  document.getElementById("dropdownSituacion").value = "";
-  document.getElementById("labelExtra").innerText = "";
-  document.getElementById("divTaulaPerjudicat").style.display = "none";
-  document.getElementById("divTaulaAutor").style.display = "none";
-  document.getElementById("textResta").style.display = "none";
-  document.getElementById("divTaulaResta").style.display = "block";
   buscar();
 }
 
 
-function filterSituacio() {
-  var iHits, iHitsAutor, iHitsPerjudicat, iHitsResta;
-  iHits = 0;
-  iHitsAutor = 0;
-  iHitsPerjudicat = 0;
-  iHitsResta = 0;
-
-  inputSituacio = document.getElementById("dropdownSituacion");
-  txtFilterSituacio = inputSituacio.value;
-
-  tableResta = document.getElementById("tableResta");
-  trResta = tableResta.getElementsByTagName("tr");
-
-  tablePerjudicat = document.getElementById("tablePerjudicat");
-  trPerjudicat = tablePerjudicat.getElementsByTagName("tr");
-
-  tableAutor = document.getElementById("tableAutor");
-  trAutor = tableAutor.getElementsByTagName("tr");
-
-  for (var j = 0; j < arraySituacion.length; j++) {
-
-    if (arraySituacion[j].situacio == txtFilterSituacio) {
-
-      if (arraySituacion[j]["actes autor"]) {
-        arrayActasAutor = arraySituacion[j]["actes autor"].split(',');
-      } else {
-        arrayActasAutor = [];
-      }
-
-      if (arraySituacion[j]["actes perjudicat"]) {
-        arrayActasPerjudicat = arraySituacion[j]["actes perjudicat"].split(',');
-      } else {
-        arrayActasPerjudicat = [];
-      }
-
-      if (arraySituacion[j]["actes resta"]) {
-        arrayActasResta = arraySituacion[j]["actes resta"].split(',');
-      } else {
-        arrayActasResta = [];
-      }
-
-
-      for (var i = 0; i < trResta.length; i++) {
-        tdCodi = trResta[i].getElementsByTagName("td")[0];
-
-        if (tdCodi) {
-          txtCodi = normalice(tdCodi.textContent || tdCodi.innerText)
-
-          if (arrayActasAutor.includes(txtCodi) || arrayActasAutor.includes(txtCodi.substring(0, txtCodi.length - 2))) {
-            trAutor[i].style.display = "";
-            iHitsAutor = iHitsAutor + 1;
-          } else {
-            trAutor[i].style.display = "none";
-          }
-
-          if (arrayActasPerjudicat.includes(txtCodi) || arrayActasPerjudicat.includes(txtCodi.substring(0, txtCodi.length - 2))) {
-            trPerjudicat[i].style.display = "";
-            iHitsPerjudicat = iHitsPerjudicat + 1;
-          } else {
-            trPerjudicat[i].style.display = "none";
-          }
-
-          if (arrayActasResta.includes(txtCodi) || arrayActasResta.includes(txtCodi.substring(0, txtCodi.length - 2))) {
-            trResta[i].style.display = "";
-            iHitsResta = iHitsResta + 1;
-          } else {
-            trResta[i].style.display = "none";
-          }
-        }
-      }
-
-    }
-  }
-
-  if (iHitsResta > 0) {
-    document.getElementById("textResta").style.display = "block";
-    document.getElementById("divTaulaResta").style.display = "block";
-  } else {
-    document.getElementById("divTaulaResta").style.display = "none";
-  }
-
-  if (iHitsPerjudicat > 0) {
-    document.getElementById("divTaulaPerjudicat").style.display = "block";
-  } else {
-    document.getElementById("divTaulaPerjudicat").style.display = "none";
-  }
-
-  if (iHitsAutor > 0) {
-    document.getElementById("divTaulaAutor").style.display = "block";
-  } else {
-    document.getElementById("divTaulaAutor").style.display = "none";
-  }
-console.log("resta: " + iHitsResta);
-console.log("perjudicat: " + iHitsPerjudicat);
-console.log("autor: " + iHitsAutor);
-
-  iHits = iHitsResta + iHitsPerjudicat + iHitsAutor;
-  return iHits;
-}
 
 function buscar() {
   var input, filter, table, tr, td, i, txtValue, iHits;
   var arrayActas;
-
-  document.getElementById("divTaulaPerjudicat").style.display = "none";
-  document.getElementById("divTaulaAutor").style.display = "none";
-  document.getElementById("divTaulaResta").style.display = "block";
-  document.getElementById("textResta").style.display = "none";
-  document.getElementById("divExtraInfo").style.display = "none";
 
   iHits = 0;
   input = document.getElementById("myInput");
@@ -145,20 +28,11 @@ function buscar() {
   input2 = document.getElementById("myInput2");
   filter2 = normalice(input2.value.toUpperCase());
 
-  inputSituacio = document.getElementById("dropdownSituacion");
-  txtFilterSituacio = inputSituacio.value;
-
-  //logic = document.getElementById("button");
-  //operator = logic.innerText;
 
   table = document.getElementById("tableResta");
   tr = table.getElementsByTagName("tr");
 
-if (txtFilterSituacio != "") {
 
-    iHits = filterSituacio();
-
-  } else {
 
     if (filter == "") {
       if (filter2 != "") {
@@ -206,7 +80,7 @@ if (txtFilterSituacio != "") {
         }
       }
     }
-  }
+
 
   if (iHits == 1) {
     document.getElementById("hitCounter").innerHTML = iHits + " coincidència."
@@ -237,8 +111,6 @@ function reset() {
   document.getElementById("myInput").value = "";
 
   document.getElementById("myInput2").value = "";
-
-  document.getElementById("dropdownSituacion").value = "";
 
   buscar();
 }
@@ -271,20 +143,6 @@ function dropdownChange() {
 
   buscar();
 
-  if (txtFilterSituacio != "") {
-    for (var j = 0; j < arraySituacion.length; j++) {
-
-      if (arraySituacion[j].situacio == txtFilterSituacio) {
-        if (arraySituacion[j].extra) {
-          if (arraySituacion[j].extra.trim() != "") {
-            document.getElementById("labelExtra").innerText = arraySituacion[j].extra;
-            document.getElementById("divExtraInfo").style.display = "block";
-          }
-        }
-      }
-    }
-
-  }
 }
 
 function pageonload() {
@@ -293,10 +151,6 @@ function pageonload() {
 
   document.getElementById("myInput").value = "";
   document.getElementById("myInput2").value = "";
-  document.getElementById("dropdownSituacion").value = "";
-  document.getElementById("labelExtra").innerText = "";
-
-  loadSituaciones();
 
   decoraTablas();
 
@@ -308,11 +162,7 @@ function pageonload() {
 function decoraTablas() {
 
   tableResta = document.getElementById("tableResta");
-  tableAutor = document.getElementById("tableAutor");
-  tablePerjudicat = document.getElementById("tablePerjudicat");
   trResta = tableResta.getElementsByTagName("tr");
-  trAutor = tableAutor.getElementsByTagName("tr");
-  trPerjudicat = tablePerjudicat.getElementsByTagName("tr");
   for (i = 0; i < trResta.length; i++) {
     tdCodi = trResta[i].getElementsByTagName("td")[0];
     if (tdCodi) {
@@ -320,38 +170,26 @@ function decoraTablas() {
         case "N":
           // code block
           trResta[i].style.backgroundColor = "#fff2f7";
-          trAutor[i].style.backgroundColor = "#fff2f7";
-          trPerjudicat[i].style.backgroundColor = "#fff2f7";
           break;
         case "A":
           // code block
           trResta[i].style.backgroundColor = "#f7fff2";
-          trAutor[i].style.backgroundColor = "#f7fff2";
-          trPerjudicat[i].style.backgroundColor = "#f7fff2";
           break;
         case "D":
           // code block
           trResta[i].style.backgroundColor = "#f2fffd";
-          trAutor[i].style.backgroundColor = "#f2fffd";
-          trPerjudicat[i].style.backgroundColor = "#f2fffd";
           break;
         case "T":
           // code block
           trResta[i].style.backgroundColor = "#f3f2ff";
-          trAutor[i].style.backgroundColor = "#f3f2ff";
-          trPerjudicat[i].style.backgroundColor = "#f3f2ff";
           break;
         case "G":
           // code block  
           trResta[i].style.backgroundColor = "#f2f5ff";
-          trAutor[i].style.backgroundColor = "#f2f5ff";
-          trPerjudicat[i].style.backgroundColor = "#f2f5ff";
           break;
         case "I":
           // code block  
           trResta[i].style.backgroundColor = "#fffff2";
-          trAutor[i].style.backgroundColor = "#fffff2";
-          trPerjudicat[i].style.backgroundColor = "#fffff2";
           break;
         default:
           // code block
@@ -359,112 +197,4 @@ function decoraTablas() {
     }
 
   }
-}
-
-function processData(allText) {
-  /*
-  0: grup
-  1: situacio
-  2: actes autor
-  3: actes victima
-  4: resta actes
-  5: extra
-  */
-  var allTextLines = allText.split(/\r\n|\n/);
-  var headers = allTextLines[0].split(';');
-  var lines = [];
-
-  for (var i = 1; i < allTextLines.length; i++) {
-    var data = allTextLines[i].split(';');
-    if (data.length == headers.length) {
-
-      csvData.push({
-        "grup": data[0],
-        "situacio": data[1],
-        "actes autor": data[2],
-        "actes perjudicat": data[3],
-        "actes resta": data[4],
-        "extra": data[5]
-      });
-
-    }
-  }
-}
-
-function loadSituaciones() {
-
-  $.ajax({
-    type: "GET",
-    url: "situacions.csv",
-    dataType: "text",
-    success: function(res) {
-
-      var count = 0; // cache the running count
-      csvArray = Papa.parse(res).data;
-
-      //TENEMOS UN ARRAY --> NECESITAMOS UN ARRAY DE OBJETOS!!
-      for (i = 1; i < csvArray.length; i++) {
-        csvData.push({});
-
-        for (j = 0; j < csvArray[0].length; j++) {
-          csvData[i - 1][csvArray[0][j]] = csvArray[i][j]
-        }
-
-      }
-
-      csvData.sort(function(a, b) {
-        /*
-        if (!a.grup) {
-          console.log("a: " + a.situacio)
-          a.grup = "Resta"
-        }
-        if (!b.grup) {
-          console.log("b: " + b.situacio)
-          b.grup = "Resta"
-        }
-        */
-        if (a.grup) {
-          if (b.grup) {
-            return a.grup.localeCompare(b.grup) || a.situacio.localeCompare(b.situacio)
-          }
-        }
-      });
-
-
-      for (var i = 0; i < csvData.length; i++) {
-        arraySituacion.push(csvData[i])
-      }
-
-        $(function() {
-          var $select = $('#dropdownSituacion');
-          var grupAnterior = "";
-          var group;
-          $.each(csvData, function() {
-            if (this.grup) {
-              if (this.grup != "") {
-
-                if (grupAnterior == this.grup) {
-
-                } else {
-                  group = $('<optgroup label="' + this.grup + '" />');
-                }
-
-                if (group) {
-                  $('<option />').html(this.situacio).appendTo(group);
-                  group.appendTo($select);
-                }
-                
-                grupAnterior = this.grup;
-              }
-            }
-          });
-
-        });
-
-      
-    }
-
-  });
-
-
 }
