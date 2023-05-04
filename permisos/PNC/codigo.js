2beea2c210c7c1e6f4f63d93466af7b267ccd0ed
zaulin <zaulin@yahoo.com>
2023-05-04 18:15:52 +0200

    Add files via upload

---
+++ permisos/PNC/codigo.js
@@ -0,0 +1,14 @@
+const version = "0.9";
+const fecha = "08/05/2022";
+
+function clickBack() {
+  window.open('../index.html', "_self")
+}
+
+
+function pageonload() {
+
+  //document.getElementById("fecha").innerText = "v." + version + " - " + fecha;
+
+}
+
