d930e4c0c86b34dd84e044ce6c4b18d8238acc92
zaulin <zaulin@yahoo.com>
2023-12-11 10:16:20 +0100

    Add files via upload

---
+++ normativa/ordenances/codigo.js
@@ -0,0 +1,52 @@
+
+const version = "0.9";
+const fecha = "08/05/2022";
+const csvData = [];
+
+function clickBack() {
+  window.open('../index.html', "_self")
+}
+
+function pageonload() {
+
+  loadEnlaces();
+  
+  document.getElementById("loading").style.display = "none";
+
+}
+
+function loadEnlaces() {
+
+  $.ajax({
+    type: "GET",
+    url: "enlaces.csv",
+    dataType: "text",
+    success: function(res) {
+
+
+      var count = 0; // cache the running count
+      csvArray = Papa.parse(res).data;
+
+      //TENEMOS UN ARRAY --> NECESITAMOS UN ARRAY DE OBJETOS!!
+      for (i = 1; i < csvArray.length; i++) {
+        csvData.push({});
+
+        for (j = 0; j < csvArray[0].length; j++) {
+          csvData[i - 1][j] = csvArray[i][j]
+        }
+
+      }
+
+      for (var i = 0; i < csvData.length; i++) {
+          fila = "<tr onclick=\"window.location.href='" + csvData[i][1] + "';\"><td>" + csvData[i][0] + "</td></tr>"
+          $('#tableMain').append(fila)
+      }
+    
+    }
+
+
+  });
+
+
+}
+
