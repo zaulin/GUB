066e149bf25da54d266083c9d6816236e4fd3061
zaulin <zaulin@yahoo.com>
2024-05-10 12:55:25 +0200

    Add files via upload

---
+++ XXX/codigo.js
@@ -0,0 +1,19 @@
+function clickBack() {
+  window.open('../index.html', "_self")
+}
+
+function clickAMB(){
+    document.getElementById("loadingBlack").style.display = "block";
+    window.open("AMB/index.html","_self")
+}
+
+function clickUDIG(){
+    document.getElementById("loadingBlack").style.display = "block";
+    window.open("UDIG/index.html","_self")
+}
+
+
+function pageonload() {
+    document.getElementById("loadingBlack").style.display = "none";
+    
+}
