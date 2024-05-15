2f8af165c67e6a8559aeb46a2b678c5886e8aa8c
zaulin <zaulin@yahoo.com>
2024-05-16 00:05:51 +0200

    Add files via upload

--- XXX/AMB/codigo.js
+++ XXX/AMB/codigo.js
@@ -1,127 +1,22 @@
-const version = "0.9";
-const fecha = "23/04/2022";
-
 function clickBack() {
-    window.open('../index.html', "_self")
+  window.open('../index.html', "_self")
 }
 
-
-function erase() {
-  reset();
+function clickAMB(){
+    document.getElementById("loadingBlack").style.display = "block";
+    window.open("AMB/index.html","_self")
 }
 
-function buscar() {
-  var input, filter, table, tr, td, i, txtValue, iHits;
-
-  iHits = 0;
-
-  inputCodi = document.getElementById("myInputCodi");
-  filterCodi = normalice(inputCodi.value.toUpperCase());
-
-  table = document.getElementById("myTable");
-  tr = table.getElementsByTagName("tr");
-
-    for (i = 0; i < tr.length; i++) {
-      hit = 0;
-      tdCodi = tr[i].getElementsByTagName("td")[0];
-      if (tdCodi) {
-        txtCodi = normalice(tdCodi.textContent || tdCodi.innerText);
-        if (txtCodi.toUpperCase().indexOf(filterCodi) > -1) {
-          hit = 1;
-        } 
-
-        if (hit==1) {
-          tr[i].style.display = "";
-          iHits = iHits + 1;
-        } else {
-          tr[i].style.display = "none";
-        }
-      }
-    }
-  
+function clickUDIG(){
+    document.getElementById("loadingBlack").style.display = "block";
+    window.open("UDIG/index.html","_self")
 }
 
-function buscarProrroga() {
-  var input, filter, table, tr, td, i, txtValue, iHits;
-
-  iHits = 0;
-
-  inputCodi = document.getElementById("inputCodiProrroga");
-  filterCodi = normalice(inputCodi.value.toUpperCase());
-
-  table = document.getElementById("myTableProrroga");
-  tr = table.getElementsByTagName("tr");
-
-    for (i = 0; i < tr.length; i++) {
-      hit = 0;
-      tdCodi = tr[i].getElementsByTagName("td")[0];
-      if (tdCodi) {
-        txtCodi = normalice(tdCodi.textContent || tdCodi.innerText);
-        if (txtCodi.toUpperCase().indexOf(filterCodi) > -1) {
-          hit = 1;
-        } 
-
-        if (hit==1) {
-          tr[i].style.display = "";
-          iHits = iHits + 1;
-        } else {
-          tr[i].style.display = "none";
-        }
-      }
-    }
-  
-}
-
-function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-}
-
-function reset(){
-  document.getElementById("myInputCodi").value = "";
-  document.getElementById("inputCodiProrroga").value = "";
-  
-  buscar();
-
-  document.getElementById("myInputCodi").focus();
-}
-
-function esconde() {
-  table = document.getElementById("myTable");
-  tr = table.getElementsByTagName("tr");
-
-  for (i = 0; i < tr.length; i++) {
-
-    for (j = 2; j < 5; j++) {
-      td = tr[i].getElementsByTagName("td")[j];
-      th = tr[i].getElementsByTagName("th")[j];
-      if (td) {
-            td.style.display = "none";
-      }
-      if (th) {
-            th.style.display = "none";
-      }  
-    }
-    
-  }
-
-  reset();
-}
-
-
-function off() {
-  document.getElementById("overlay").style.display = "none";
-}
-
-function tableClick(el) {
- 
-}
-
-function normalice(text){
-  return text.normalize("NFD").replace(/[\u0300-\u036f]/g, "")
-}
 
 function pageonload() {
-  //document.getElementById("fecha").innerText = "v." + version + " - " + fecha;
-  //esconde();
-  document.getElementById("loading").style.display = "none";
+    //document.getElementById("loadingBlack").style.display = "none";
+}
+
+function test() {
+    document.getElementById("loadingBlack").style.display = "none";
 }
