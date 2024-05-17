7d15c31aa0e99bcef3eab0fb07a48ac8253c4def
zaulin <zaulin@yahoo.com>
2024-05-17 22:51:07 +0200

    Add files via upload

--- XXX/codigo.js
+++ XXX/codigo.js
@@ -3,17 +3,45 @@ function clickBack() {
 }
 
 function clickAMB(){
-    document.getElementById("loadingBlack").style.display = "block";
+    if (getMobileOperatingSystem() != "Android") {
+        document.getElementById("loadingBlack").style.display = "block";
+    }
     window.open("AMB/index.html","_self")
 }
 
 function clickUDIG(){
-    document.getElementById("loadingBlack").style.display = "block";
+    if (getMobileOperatingSystem() != "Android") {
+        document.getElementById("loadingBlack").style.display = "block";
+    }
     window.open("UDIG/index.html","_self")
 }
 
 
 function pageonload() {
+    //document.getElementById("loadingBlack").style.display = "none";
+}
+
+function test() {
     document.getElementById("loadingBlack").style.display = "none";
-    
+}
+
+
+function getMobileOperatingSystem() {
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+    // Windows Phone must come first because its UA also contains "Android"
+    if (/windows phone/i.test(userAgent)) {
+        return "Windows Phone";
+    }
+
+    if (/android/i.test(userAgent)) {
+        return "Android";
+    }
+
+    // iOS detection from: http://stackoverflow.com/a/9039885/177710
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+        return "iOS";
+    }
+
+    return "unknown";
 }
