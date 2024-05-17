5fd307b68fdcd17091b7bbf8b647d4a60339565c
zaulin <zaulin@yahoo.com>
2024-05-17 22:53:39 +0200

    Add files via upload

--- normativa/codigo.js
+++ normativa/codigo.js
@@ -3,32 +3,63 @@ function clickBack() {
 }
 
 function clickOrdenances(){
-    document.getElementById("loadingBlack").style.display = "block";
+    if (getMobileOperatingSystem() != "Android") {
+        document.getElementById("loadingBlack").style.display = "block";
+    }
     window.open("ordenances/index.html","_self")
 }
 
 function clickCirculacio(){
-    document.getElementById("loadingBlack").style.display = "block";
+    if (getMobileOperatingSystem() != "Android") {
+        document.getElementById("loadingBlack").style.display = "block";
+    }
     window.open("circulacio/index.html","_self")
 }
 
 function clickConvivencia(){
-    document.getElementById("loadingBlack").style.display = "block";
+    if (getMobileOperatingSystem() != "Android") {
+        document.getElementById("loadingBlack").style.display = "block";
+    }
     window.open("convivencia/index.html","_self")
 }
 
 
 function clickVTC(){
-    document.getElementById("loadingBlack").style.display = "block";
+    if (getMobileOperatingSystem() != "Android") {
+        document.getElementById("loadingBlack").style.display = "block";
+    }
     window.open("VTC/index.html","_self")
 }
 
 function clickPlatges(){
-    document.getElementById("loadingBlack").style.display = "block";
+    if (getMobileOperatingSystem() != "Android") {
+        document.getElementById("loadingBlack").style.display = "block";
+    }
     window.open("platges/index.html","_self")
 }
 
 function pageonload() {
-    document.getElementById("loadingBlack").style.display = "none";
+    //document.getElementById("loadingBlack").style.display = "none";
     
+}
+
+
+function getMobileOperatingSystem() {
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+    // Windows Phone must come first because its UA also contains "Android"
+    if (/windows phone/i.test(userAgent)) {
+        return "Windows Phone";
+    }
+
+    if (/android/i.test(userAgent)) {
+        return "Android";
+    }
+
+    // iOS detection from: http://stackoverflow.com/a/9039885/177710
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+        return "iOS";
+    }
+
+    return "unknown";
 }
