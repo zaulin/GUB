59d95d461e688e92644be0fddfb95e6b9929bf40
zaulin <zaulin@yahoo.com>
2024-05-17 22:52:13 +0200

    Add files via upload

--- actuacions/transit/codigo.js
+++ actuacions/transit/codigo.js
@@ -3,31 +3,62 @@ function clickBack() {
 }
 
 function clickCotxeMoto(){
-    document.getElementById("loadingBlack").style.display = "block";
+    if (getMobileOperatingSystem() != "Android") {
+        document.getElementById("loadingBlack").style.display = "block";
+    }
     window.open("cotxeMoto/index.html","_self")
 }
 
 function clickVmp(){
-    document.getElementById("loadingBlack").style.display = "block";
+    if (getMobileOperatingSystem() != "Android") {
+        document.getElementById("loadingBlack").style.display = "block";
+    }
     window.open("vmp/index.html","_self")
 }
 
 function clickVTC(){
-    document.getElementById("loadingBlack").style.display = "block";
+    if (getMobileOperatingSystem() != "Android") {
+        document.getElementById("loadingBlack").style.display = "block";
+    }
     window.open("VTC/index.html","_self")
 }
 
 function clickPNC(){
-    document.getElementById("loadingBlack").style.display = "block";
+    if (getMobileOperatingSystem() != "Android") {
+        document.getElementById("loadingBlack").style.display = "block";
+    }
     window.open("PNC/index.html","_self")
 }
 
 function clickEstrangers(){
-    document.getElementById("loadingBlack").style.display = "block";
+    if (getMobileOperatingSystem() != "Android") {
+        document.getElementById("loadingBlack").style.display = "block";
+    }
     window.open("permisos/index.html","_self")
 }
 
 function pageonload() {
-    document.getElementById("loadingBlack").style.display = "none";
+    //document.getElementById("loadingBlack").style.display = "none";
     
+}
+
+
+function getMobileOperatingSystem() {
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+    // Windows Phone must come first because its UA also contains "Android"
+    if (/windows phone/i.test(userAgent)) {
+        return "Windows Phone";
+    }
+
+    if (/android/i.test(userAgent)) {
+        return "Android";
+    }
+
+    // iOS detection from: http://stackoverflow.com/a/9039885/177710
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+        return "iOS";
+    }
+
+    return "unknown";
 }
